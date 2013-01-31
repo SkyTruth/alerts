333dd1252334a06689f58ee84f349e48b5c63015
pwoods25443 <paul@skytruth.org>
2013-01-31 13:11:04 -0500

    missing semicolon

--- www/includes/config_template.php
+++ www/includes/config_template.php
@@ -15,7 +15,7 @@ $CONFIG['FEED_BASE_URL'] = $CONFIG['BASE_URL'];
 $CONFIG['SUBSCRIBE_URL'] = $CONFIG['BASE_URL'] . 'subscribe';
 $CONFIG['REGION_KML_BASE_URL'] = $CONFIG['BASE_URL'] . 'region/';
 
-$CONFIG['MAP_COOKIE_NAME'] = 'alerts_map'
+$CONFIG['MAP_COOKIE_NAME'] = 'alerts_map';
 
 $CONFIG['GA_ACCOUNT'] = 'UA-25593503-1';
 $CONFIG['GA_DOMAIN'] = '.skytruth.org';
